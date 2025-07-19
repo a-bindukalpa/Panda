4cab9cbd7754baadadc42e0b5d733c134e0d6019
a-bindukalpa <abindukalpa@gmail.com>
2025-07-19 16:06:26 +0100

    (feat-8):appointments integration tests (#18)
    Co-authored-by: Amrita Bindukalpa <amrita.bindukalpa@gmail.com>

--- Panda.API/Panda.API/Program.cs
+++ Panda.API/Panda.API/Program.cs
@@ -3,49 +3,53 @@ using MongoDB.Driver;
 using Persistence;
 using Service;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddPersistence();
-builder.Services.AddSingleton<IMongoDatabase>(sp =>
+public class Program
 {
-    var configuration = sp.GetRequiredService<IConfiguration>();
-    var connectionString = configuration.GetConnectionString("MongoDb");
-    var databaseName = configuration["MongoDbSettings:DatabaseName"];
-    var client = new MongoClient(connectionString);
-    return client.GetDatabase(databaseName);
-});
-builder.Services.AddScoped<IMongoCollection<Patient>>(sp =>
-{
-    var database = sp.GetRequiredService<IMongoDatabase>();
-    return database.GetCollection<Patient>("Patients");
-});
-builder.Services.AddScoped<IMongoCollection<Appointment>>(sp =>
-{
-    var database = sp.GetRequiredService<IMongoDatabase>();
-    return database.GetCollection<Appointment>("Appointments");
-});
-builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
+        builder.Services.AddPersistence();
+        builder.Services.AddSingleton<IMongoDatabase>(sp =>
+        {
+            var configuration = sp.GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionString("MongoDb");
+            var databaseName = configuration["MongoDbSettings:DatabaseName"];
+            var client = new MongoClient(connectionString);
+            return client.GetDatabase(databaseName);
+        });
+        builder.Services.AddScoped<IMongoCollection<Patient>>(sp =>
+        {
+            var database = sp.GetRequiredService<IMongoDatabase>();
+            return database.GetCollection<Patient>("Patients");
+        });
+        builder.Services.AddScoped<IMongoCollection<Appointment>>(sp =>
+        {
+            var database = sp.GetRequiredService<IMongoDatabase>();
+            return database.GetCollection<Appointment>("Appointments");
+        });
+        builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app
+            .UseHttpsRedirection()
+            .UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app
-    .UseHttpsRedirection()
-    .UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
